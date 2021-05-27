7fe15c305c68dcb26401306d734f371764824610
Lim Min Kuan <minkuan.lim@idemia.com>
2021-05-27 15:54:18 +0800

    Add encryption for kSecKeyAlgorithmRSAEncryptionOAEPSHA256AESGCM

---
+++ applecryptointeroperability/src/main/java/company/ize/applecryptointeroperability/RSAKey.kt
@@ -0,0 +1,58 @@
+package company.ize.applecryptointeroperability
+
+import android.security.keystore.KeyProperties
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo
+import java.math.BigInteger
+import java.security.KeyFactory
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.interfaces.RSAPrivateCrtKey
+import java.security.spec.RSAPrivateKeySpec
+import java.security.spec.RSAPublicKeySpec
+
+//  Created by Min Kuan LIM on 27/05/2021.
+//  Copyright © IZE. All rights reserved.
+//  See LICENSE.txt for licensing information.
+//
+
+/**
+ * Return the default BER or DER encoding for this key.
+ */
+fun PublicKey.getAsn1Primitive(): ByteArray = SubjectPublicKeyInfo
+    .getInstance(encoded)
+    .parsePublicKey()
+    .encoded
+
+/**
+ * Derive the public key spec by first attempting to convert
+ * this key into a [RSAPrivateCrtKey].
+ *
+ * If it fails, attempt to derive the public exponent by using
+ * the most common value (65537).
+ *
+ * Note: This is not a fool proof solution and can only yield a probabilistic result
+ */
+fun PrivateKey.derivePublicKeySpec(): RSAPublicKeySpec =
+    if (this is RSAPrivateCrtKey) {
+        RSAPublicKeySpec(this.modulus, this.publicExponent)
+    } else {
+        val keyFactory = KeyFactory.getInstance(KeyProperties.KEY_ALGORITHM_RSA)
+        val rsaPrivateKeySpec = keyFactory.getKeySpec(this, RSAPrivateKeySpec::class.java)
+        // Making a wild guess on what might be the public exponent
+        // by using the most common value (65537)
+        RSAPublicKeySpec(
+            rsaPrivateKeySpec.modulus,
+            BigInteger.valueOf(65537)
+        )
+    }
+
+/**
+ * Get the derived public key
+ *
+ * @see [derivePublicKeySpec]
+ */
+fun PrivateKey.derivePublicKey(): PublicKey {
+    val keyFactory = KeyFactory.getInstance(KeyProperties.KEY_ALGORITHM_RSA)
+    val publicKeySpec = derivePublicKeySpec()
+    return keyFactory.generatePublic(publicKeySpec)
+}
